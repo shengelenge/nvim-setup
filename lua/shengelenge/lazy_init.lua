b558202557eff625484ca37859eff95c8327eb1c
Nicolas Wielocha <nicolas@wielocha.pl>
2024-03-19 17:08:39 +0100

    Gooood changes

--- lua/shengelenge/lazy_init.lua
+++ lua/shengelenge/lazy_init.lua
@@ -11,7 +11,10 @@ if not vim.loop.fs_stat(lazypath) then
 end
 vim.opt.rtp:prepend(lazypath)
 
-require("lazy").setup("shengelenge.lazy")
+require("lazy").setup({
+    spec = "shengelenge.lazy",
+    change_detection = { notify = false }
+})
     --[[
 	use({
 		"VonHeikemen/lsp-zero.nvim",
