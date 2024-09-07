412bf9ef1a029c9242b59454691cce96778080a5
Nicolas Wielocha <nicolas@wielocha.pl>
2024-09-07 13:21:15 +0200

    fix: conform stop_after_first

--- lua/shengelenge/lazy/conform.lua
+++ lua/shengelenge/lazy/conform.lua
@@ -1,39 +1,40 @@
 return {
-    "stevearc/conform.nvim",
-    event = { "BufReadPre", "BufNewFile" },
-    config = function()
-        local conform = require("conform")
+	"stevearc/conform.nvim",
+	event = { "BufReadPre", "BufNewFile" },
 
-        conform.setup({
-            formatters_by_ft = {
-                cpp = { "clang_format" },
-                javascript = { { "prettierd", "prettier" } },
-                typescript = { { "prettierd", "prettier" } },
-                javascriptreact = { { "prettierd", "prettier" } },
-                typescriptreact = { { "prettierd", "prettier" } },
-                json = { { "prettierd", "prettier" } },
-                yaml = { { "prettierd", "prettier" } },
-                markdown = { { "prettierd", "prettier" } },
-                html = { { "prettierd", "prettier" } },
-                css = { { "prettierd", "prettier" } },
-                scss = { { "prettierd", "prettier" } },
-                lua = { "stylua" },
-                python = { "isort", "black" },
-                go = { "gopls" },
-            },
-            format_on_save = {
-                lsp_fallback = true,
-                async = false,
-                timeout_ms = 500,
-            },
-        })
+	config = function()
+		local conform = require("conform")
 
-        vim.keymap.set("n", "<leader>f", function()
-            conform.format({
-                lsp_fallback = true,
-                async = false,
-                timeout_ms = 500,
-            })
-        end, { desc = "Format file or range (in visual mode)" })
-    end,
+		conform.setup({
+			formatters_by_ft = {
+				cpp = { "clang_format" },
+				javascript = { { "prettierd", "prettier", stop_after_first = true } },
+				typescript = { { "prettierd", "prettier", stop_after_first = true } },
+				javascriptreact = { { "prettierd", "prettier", stop_after_first = true } },
+				typescriptreact = { { "prettierd", "prettier", stop_after_first = true } },
+				json = { { "prettierd", "prettier", stop_after_first = true } },
+				yaml = { { "prettierd", "prettier", stop_after_first = true } },
+				markdown = { { "prettierd", "prettier", stop_after_first = true } },
+				html = { { "prettierd", "prettier", stop_after_first = true } },
+				css = { { "prettierd", "prettier", stop_after_first = true } },
+				scss = { { "prettierd", "prettier", stop_after_first = true } },
+				lua = { "stylua" },
+				python = { "isort", "black", stop_after_first = true },
+				go = { "gopls" },
+			},
+			format_on_save = {
+				lsp_fallback = true,
+				async = false,
+				timeout_ms = 500,
+			},
+		})
+
+		vim.keymap.set("n", "<leader>f", function()
+			conform.format({
+				lsp_fallback = true,
+				async = false,
+				timeout_ms = 500,
+			})
+		end, { desc = "Format file or range (in visual mode)" })
+	end,
 }
